cc3011faaaba47be9bc765088cd802be06ae360a
Zyxary <kelpselp@gmail.com>
2026-01-04 18:31:44 +0800

    Loops: proj 3

--- C-modern-approach-projects/Loops/frac-lowest-term.c
+++ C-modern-approach-projects/Loops/frac-lowest-term.c
@@ -7,9 +7,31 @@ Hint: To reduce a fraction to lowest terms, first compute the GCD of the numerat
 denominator. Then divide both the numerator and denominator by the GCD.
 */
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
+    int m, n;
+    int denom, nume;
+
+    printf("Enter a fraction: ");
+    scanf("%d / %d", &m, &n);
+
+    nume = m;
+    denom = n;
+
+    while(n != 0)
+    {
+        int r = m % n;
+        m = n;
+        n = r;
+    }
+    nume = nume / m;
+    denom = denom / m;
+
+    printf("Lowest terms: %d/%d", nume, denom);
     
     return 0;
 }
+
+// TTC: 11:18
