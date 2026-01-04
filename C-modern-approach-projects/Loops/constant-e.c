3ed8577ae0fb8b70aff80a669974546875a79785
Zyxary <kelpselp@gmail.com>
2026-01-04 19:40:05 +0800

    Loops: proj 11

---
+++ C-modern-approach-projects/Loops/constant-e.c
@@ -0,0 +1,44 @@
+/*
+The value of the mathematical constant
+e can be expressed as an infinite series:
+e = 1 4- 1/11+ 1/2! 4- 1/3! 4-...
+Write a program that approximates e by computing the value of
+14- 1/114- 1/2! 4- 1/3! 4- ... + l/n!
+where n is an integer entered by the user.
+*/
+
+#include <stdio.h>
+
+int factIter(int product, int counter, int maxCount)
+{
+    if(counter > maxCount) return product;
+    else
+    {
+        return factIter((counter * product), (counter + 1), maxCount);
+    }
+}
+
+int factorial(int n)
+{
+    return factIter(1, 1, n);
+}
+
+int main(void)
+{
+    int n;
+    double e = 1.0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        double num = 1.0 / (factorial(i));
+        e += num;
+    }
+    printf("e = %f", e);
+
+    return 0;
+}
+
+
+//TTC: 27:48
